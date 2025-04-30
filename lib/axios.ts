fe40520ff8fedb2504b2db24e96b1ecf685797e7
sujalmh9@gmail.com <sujalmh>
2025-04-30 21:10:52 +0530

    Test

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://job-portal-backend-zt28.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
